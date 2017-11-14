e68589583b455b0d9f36edff9b5701211c0d17f9
Avery VanKirk <fredknex@gmail.com>
2017-11-13 20:27:47 -0800

    ex 2.1

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -13,4 +13,6 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+func FToK(f Fahrenheit) Kelvin { return Kelvin((f - 32) * 5 / 9 + 100) }
+
 //!-
